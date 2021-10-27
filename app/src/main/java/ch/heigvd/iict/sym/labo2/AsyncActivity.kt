50bbae208e5d94a503cec49de655461b13a223c0
Ilias <igoujgali@gmail.com>
2021-10-27 19:05:12 +0200

    async

--- app/src/main/java/ch/heigvd/iict/sym/labo2/AsyncActivity.kt
+++ app/src/main/java/ch/heigvd/iict/sym/labo2/AsyncActivity.kt
@@ -2,10 +2,28 @@ package ch.heigvd.iict.sym.labo2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class AsyncActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_async)
+        val sendBtn: Button = findViewById(R.id.sendBtn)
+        val requestInput  : EditText = findViewById(R.id.requestTextInput)
+        val responseOutput  : TextView = findViewById(R.id.responseTextOutput)
+        val sym = SymComManager(object : CommunicationEventListener {
+            override fun handleServerResponse(response: String) {
+                responseOutput.text = response
+            }
+        })
+        sendBtn.setOnClickListener {
+            sym.sendRequest("http://mobile.iict.ch/api/txt", SymComManager.ContentType.TEXT_PLAIN, requestInput.text.toString())
+        }
     }
 }
--- app/src/main/java/ch/heigvd/iict/sym/labo2/SymComManager.kt
+++ app/src/main/java/ch/heigvd/iict/sym/labo2/SymComManager.kt
@@ -1,9 +1,54 @@
 package ch.heigvd.iict.sym.labo2
 
-class SymComManager(var communicationEventListener: CommunicationEventListener? = null) {
+import android.os.Build
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.Looper
+import android.util.Log
+import androidx.annotation.RequiresApi
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.URL
 
-    fun sendRequest(url: String, request: String) {
-		//TODO to implement
+class SymComManager(var communicationEventListener: CommunicationEventListener? = null) {
+    public enum class ContentType(val text: String) {
+        TEXT_PLAIN("text/plain"),
+        JSON("application/json");
+
+        override fun toString(): String {
+            return text
+        }
+    }
+
+    fun sendRequest(url: String, contentType: ContentType, request: String) {
+        val handler = Handler(Looper.myLooper()!!)
+        Thread() {
+            run {
+                val urlObject = URL(url)
+                val httpConnection = urlObject.openConnection() as HttpURLConnection
+                httpConnection.requestMethod = "POST"
+                httpConnection.setRequestProperty("Content-Type", contentType.toString());
+                httpConnection.doOutput = true
+
+                try {
+                    val bufferWriter = httpConnection.outputStream.bufferedWriter()
+                    bufferWriter.write(request)
+                    bufferWriter.flush()
+
+                    val str = StringBuilder()
+                    httpConnection.inputStream.bufferedReader().lines().forEach(str::append)
+                    
+                    handler.post {
+                        run {
+                            communicationEventListener?.handleServerResponse(str.toString())
+                        }
+                    }
+                } finally {
+                    httpConnection.disconnect()
+                }
+
+            }
+        }.start()
     }
 
 }
