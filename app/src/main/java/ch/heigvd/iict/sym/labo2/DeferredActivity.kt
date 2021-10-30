55b74e4b9fa0536b9fc63609c5b2af7d5bc2d06f
Ilias <igoujgali@gmail.com>
2021-10-30 11:09:31 +0200

    Différé

--- app/src/main/java/ch/heigvd/iict/sym/labo2/DeferredActivity.kt
+++ app/src/main/java/ch/heigvd/iict/sym/labo2/DeferredActivity.kt
@@ -2,10 +2,26 @@ package ch.heigvd.iict.sym.labo2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class DeferredActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_deferred)
+        val sendBtn: Button = findViewById(R.id.sendBtn)
+        val requestInput  : EditText = findViewById(R.id.requestTextInput)
+        val responseOutput  : TextView = findViewById(R.id.responseTextOutput)
+        val sym = SymComManager(object : CommunicationEventListener {
+            override fun handleServerResponse(response: String) {
+                val text : String = responseOutput.text.toString()
+                // TODO enelever la suggestion
+                responseOutput.text = text + response + "\n"
+            }
+        })
+        sendBtn.setOnClickListener {
+            sym.sendRequest("http://mobile.iict.ch/api/txt", SymComManager.ContentType.TEXT_PLAIN, requestInput.text.toString())
+        }
     }
 }
--- app/src/main/java/ch/heigvd/iict/sym/labo2/SymComManager.kt
+++ app/src/main/java/ch/heigvd/iict/sym/labo2/SymComManager.kt
@@ -9,46 +9,77 @@ import androidx.annotation.RequiresApi
 import java.lang.StringBuilder
 import java.net.HttpURLConnection
 import java.net.URL
+import java.net.UnknownHostException
+import java.util.*
 
 class SymComManager(var communicationEventListener: CommunicationEventListener? = null) {
+
+    private var pendingRequests: MutableList<Request> = arrayListOf();
+
+    init {
+        Timer().schedule(object : TimerTask() {
+            override fun run() {
+                if (hasPendingRequest()) {
+                    val req: Request = pendingRequests.removeFirst()
+                    sendRequest(req.url, req.contentType, req.request)
+                }
+            }
+        }, 5000, 5000);
+    }
+
     public enum class ContentType(val text: String) {
         TEXT_PLAIN("text/plain"),
-        JSON("application/json");
+        JSON("application/json"),
+        XML("application/xml");
+
 
         override fun toString(): String {
             return text
         }
     }
 
+    fun hasPendingRequest(): Boolean {
+        return pendingRequests.isNotEmpty()
+    }
+
     fun sendRequest(url: String, contentType: ContentType, request: String) {
-        val handler = Handler(Looper.myLooper()!!)
+        val handler = Handler(Looper.getMainLooper()!!)
         Thread() {
             run {
                 val urlObject = URL(url)
-                val httpConnection = urlObject.openConnection() as HttpURLConnection
-                httpConnection.requestMethod = "POST"
-                httpConnection.setRequestProperty("Content-Type", contentType.toString());
-                httpConnection.doOutput = true
-
+                val httpConnection = urlObject.openConnection() as HttpURLConnection;
                 try {
+
+
+                    httpConnection.requestMethod = "POST"
+                    httpConnection.setRequestProperty(
+                        "Content-Type",
+                        contentType.toString()
+                    );
+                    httpConnection.doOutput = true
+
+
                     val bufferWriter = httpConnection.outputStream.bufferedWriter()
                     bufferWriter.write(request)
                     bufferWriter.flush()
 
+
                     val str = StringBuilder()
                     httpConnection.inputStream.bufferedReader().lines().forEach(str::append)
-                    
+
                     handler.post {
                         run {
                             communicationEventListener?.handleServerResponse(str.toString())
                         }
+
                     }
+                    
+                } catch (unknownHostEx: UnknownHostException) {
+                    pendingRequests.add(Request(url, contentType, request))
                 } finally {
                     httpConnection.disconnect()
                 }
-
             }
         }.start()
     }
-
 }
